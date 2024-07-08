990da94b4980f5ee27a194667ba3f3628574825f
ahmetaydin0801 <ahmet.aydin@pubinno.com>
2024-07-09 00:29:26 +0300

    run null :false migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_07_200619) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_08_212750) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "books", force: :cascade do |t|
     t.bigint "user_id", null: false
-    t.string "title"
-    t.text "description"
+    t.string "title", null: false
+    t.text "description", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_books_on_user_id"
