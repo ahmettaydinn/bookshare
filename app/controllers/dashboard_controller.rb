ea19a22221bf942df01a5b578724ae8d3502d845
ahmetaydin0801 <ahmet.aydin@pubinno.com>
2024-07-20 22:19:31 +0300

    Add a search bar in dashboard page for books

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
   def index
-    @books = Book.includes(:user).all
+    @books = Book.includes(:user) # Load all books with their associated users
+
+    if params[:search].present?
+      search_query = params[:search].downcase
+      @books = @books.where('lower(title) LIKE ? OR lower(description) LIKE ?', "%#{search_query}%", "%#{search_query}%")
+    end
   end
 end
