0b4c9e0e8c3ca5ac181e251b7d6ac57d2d5de8c3
ahmetaydin0801 <ahmet.aydin@pubinno.com>
2024-07-27 19:59:30 +0300

    create error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,23 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
-end
+  
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+    rescue_from StandardError, with: :standard_error
+  
+    private
+  
+    def record_not_found
+      flash[:alert] = 'Record not found.'
+      redirect_to root_path
+    end
+  
+    def standard_error(exception)
+      # Log the error for debugging
+      Rails.logger.error(exception.message)
+      Rails.logger.error(exception.backtrace.join("\n"))
+  
+      flash[:alert] = 'An unexpected error occurred. Please try again later.'
+      redirect_to root_path
+    end
+  end
+  
